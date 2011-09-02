a65227dc67bfbf415c333f6cf15ca7e33fd32ebe
fumiyas <fumiyas@69f2458b-35d4-408a-aa88-7d8d64836e89>
2011-09-02 06:16:07 +0000

    smbldap-upgrade-0.9.6: Show diagnosis message on error
    git-svn-id: svn+ssh://svn.gna.org/svn/smbldap-tools/trunk@93 69f2458b-35d4-408a-aa88-7d8d64836e89

--- smbldap-upgrade-0.9.6.pl
+++ smbldap-upgrade-0.9.6.pl
@@ -1,28 +1,49 @@
 #!@PERL_CMD@
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use warnings;
 use smbldap_tools;
 
-my $attr = "sambaAlgorithmicRidBase";
+my $base_attr = "sambaAlgorithmicRidBase";
+my $base_value = 1000;
 
 my $ldap = eval { connect_ldap_master(); };
-## Not configured?
-$@ && exit(0);
+if ($@) {
+    ## Not configured?
+    die "$0: ERROR: Connecting to LDAP master failed: $@\n";
+}
 
 my $search = $ldap->search(
     base => $config{suffix},
     filter => "(objectClass=sambaDomain)",
     scope => "sub",
-    attrs => [$attr],
+    attrs => [$base_attr],
 );
-## Not configured?
-$search->code && exit(0);
+if ($search->code) {
+    ## Not configured?
+    die "$0: ERROR: Searching for sambaDomain object failed: " . $search->error . "\n";
+}
 
 for my $entry ($search->entries) {
-    next if (defined($entry->get_value($attr)));
-    my $modify = $ldap->modify($entry->dn, replace => [$attr => 1000]);
-    $modify->code && die "$0: ERROR: Cannot add $attr to " . $entry->dn;
+    next if (defined($entry->get_value($base_attr)));
+    print "$0: Adding $base_attr to " . $entry->dn . " ...\n";
+    my $modify = $ldap->modify($entry->dn, add => [$base_attr => $base_value]);
+    if ($modify->code) {
+	die "$0: ERROR: Cannot add $base_attr to " . $entry->dn . ": " . $modify->error . "\n";
+    }
 }
 
 exit(0);
